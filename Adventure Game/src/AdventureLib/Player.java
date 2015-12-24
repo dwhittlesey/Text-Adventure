77407e2caf89f219d9905a26cff8e9bb3c1e6f80
dwhittlesey <d.j.whittlesey@gmail.com>
2015-12-23 22:16:14 -0500

    test

---
+++ Adventure Game/src/AdventureLib/Player.java
@@ -0,0 +1,20 @@
+package AdventureLib;
+
+import java.util.Date;
+
+public class Player {
+	String Name = "";
+	Date dateCreated  = new Date();
+	int Health = 100;
+	int Stamina = 10;
+	int Perception = 10;
+	int Endurance = 10;
+	int Charisma = 10;
+	int Speed = 10;
+	int Encumberance = 0;
+	int strength = 10;
+	int Saiety = 100;
+	
+	
+
+}
---
+++ Adventure Game/src/AdventureLib/TextAdventure.java
@@ -0,0 +1,8 @@
+package AdventureLib;
+
+import java.util.Date;
+
+public class TextAdventure {
+		
+
+}
