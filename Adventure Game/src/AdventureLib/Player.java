13aeb002277f1145e208cb3daa1ce40cb2fd7c48
dwhittlesey <d.j.whittlesey@gmail.com>
2015-12-24 20:44:07 -0500

    updated player object

--- Adventure Game/src/AdventureLib/Player.java
+++ Adventure Game/src/AdventureLib/Player.java
@@ -3,17 +3,80 @@ package AdventureLib;
 import java.util.Date;
 
 public class Player {
-	String Name = "";
+	
 	Date dateCreated  = new Date();
-	int Health = 100;
-	int Stamina = 10;
-	int Perception = 10;
-	int Endurance = 10;
-	int Charisma = 10;
-	int Speed = 10;
-	int Encumberance = 0;
-	int strength = 10;
-	int Saiety = 100;
+	private String Name = "";
+	private int Health = 100;
+	private int Stamina = 10;
+	private int Perception = 10;
+	private int Endurance = 10;
+	private int Charisma = 10;
+	private int Speed = 10;
+	private int Encumberance = 0;
+	private int strength = 10;
+	private int Saiety = 100;
+	int getHealth() {
+		return Health;
+	}
+	void setHealth(int health) {
+		Health = health;
+	}
+	int getSaiety() {
+		return Saiety;
+	}
+	void setSaiety(int saiety) {
+		Saiety = saiety;
+	}
+	int getStrength() {
+		return strength;
+	}
+	void setStrength(int strength) {
+		this.strength = strength;
+	}
+	int getEncumberance() {
+		return Encumberance;
+	}
+	void setEncumberance(int encumberance) {
+		Encumberance = encumberance;
+	}
+	int getSpeed() {
+		return Speed;
+	}
+	void setSpeed(int speed) {
+		Speed = speed;
+	}
+	int getCharisma() {
+		return Charisma;
+	}
+	void setCharisma(int charisma) {
+		Charisma = charisma;
+	}
+	int getEndurance() {
+		return Endurance;
+	}
+	void setEndurance(int endurance) {
+		Endurance = endurance;
+	}
+	int getPerception() {
+		return Perception;
+	}
+	void setPerception(int perception) {
+		Perception = perception;
+	}
+	int getStamina() {
+		return Stamina;
+	}
+	void setStamina(int stamina) {
+		Stamina = stamina;
+	}
+	String getName() {
+		return Name;
+	}
+	void setName(String name) {
+		Name = name;
+	}
+	
+	
 	
 	
 
